8ad7147b3260de518e3c52c551f38e4ab0be8c9f
Amir Salama <hamirsalama@gmail.com>
2025-02-15 01:40:06 +0200

    falcon

--- resources/views/About/index.blade.php
+++ resources/views/About/index.blade.php
@@ -5,7 +5,8 @@
     <ul role="list" class="divide-y divide-gray-100">
         <li class="flex justify-between gap-x-6 py-5">
           <div class="flex min-w-0 gap-x-4">
-            <img class="size-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+            {{-- <img class="size-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""> --}}
+            <img class="size-12 flex-none rounded-full bg-gray-50" src="/WhatsApp Image 2025-01-16 at 3.06.43 AM.jpeg" alt="">
             <div class="min-w-0 flex-auto">
               <p class="text-sm/6 font-semibold text-gray-900">Amir Salama Hassan</p>
               <p class="mt-1 truncate text-xs/5 text-gray-500">hamirsalama@gmail.com</p>
@@ -13,7 +14,12 @@
           </div>
           <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
             <p class="text-sm/6 text-gray-900">Back-End Developer</p>
-            <p class="mt-1 text-xs/5 text-gray-500">Last seen <time datetime="2023-01-23T13:23Z">8h ago</time></p>
+            <div class="mt-1 flex items-center gap-x-1.5">
+              <div class="flex-none rounded-full bg-emerald-500/20 p-1">
+                <div class="size-1.5 rounded-full bg-emerald-500"></div>
+              </div>
+              <p class="text-xs/5 text-gray-500">Online</p>
+            </div>
           </div>
         </li>
         <li class="flex justify-between gap-x-6 py-5">
